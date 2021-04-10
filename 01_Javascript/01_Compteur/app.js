07981b91104dfe5824c77490dc131483e0551ea7
mostaphaelarfaoui <mostapha0411@gmail.com>
2021-04-10 21:15:12 +0100

    add javascript (Compteur)

---
+++ 01_Javascript/01_Compteur/app.js
@@ -0,0 +1,27 @@
+let add = document.getElementById('indecrease');
+let remove = document.getElementById('decrease');
+let reset = document.getElementById('reset');
+let int = document.getElementById('value');
+let integer = 0;
+add.addEventListener('click',function(){
+    integer +=1;
+    int.innerHTML =integer;
+    if(integer>=1){
+    int.style.color = 'green';
+}
+})
+remove.addEventListener('click',function(){
+    integer -=1 ;
+    int.innerHTML =integer;
+    if(integer<0){
+        int.style.color = 'red';
+    }
+})
+reset.addEventListener('click',function(){
+    integer =0;
+    int.innerHTML =integer;
+    int.style.color = 'black';
+    
+        
+    
+})
