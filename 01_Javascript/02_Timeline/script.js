337cb04389b69b195b889ad0d9b9e4f6dd91b021
mostaphaelarfaoui <mostapha0411@gmail.com>
2021-04-13 09:22:26 +0000

    add time line

--- 01_Javascript/02_Timeline/script.js
+++ 01_Javascript/02_Timeline/script.js
@@ -1,1 +1,23 @@
-// Votre code javascript
+
+var move = document.querySelectorAll(".timeline li");
+
+
+function element(e) {
+  var div = e.getBoundingClientRect();
+  return (
+    div.bottom <= window.innerHeight  &&
+    div.top >= 0 
+  );
+}
+
+function call() {
+  for (var i = 0; i < move.length; i++) {
+    if (element(move[i])) {
+      move[i].classList.add("show");
+    }
+  }
+}
+
+
+document.addEventListener("scroll", call);
+
